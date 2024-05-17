85bdcb9190c93dd07bda6018c9dce69536d93be4
Dhruv <dhruv.verma8@gmail.com>
2024-05-17 13:15:59 +0530

    initial front-end done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [dateTime, setDateTime] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>
+        ₹400 <span>.00</span>
+      </h1>
+      <form>
+        <div className="basic">
+          <input
+            type="text"
+            placeholder="+200 for Petrol"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="datetime-local"
+            value={dateTime}
+            onChange={(e) => setDateTime(e.target.value)}
+          />
+        </div>
+        <div className="description">
+          <input
+            type="text"
+            placeholder={"description"}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <button type="submit">Add new Transaction</button>
+      </form>
+      <div className="transactions">
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New Samsung TV</div>
+            <div className="description">it was time for a new TV</div>
+          </div>
+          <div className="right">
+            <div className="price red">-₹40000</div>
+            <div className="dateTime">2024-05-17 01:00</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">Gig job new website</div>
+            <div className="description">it was time for a new TV</div>
+          </div>
+          <div className="right">
+            <div className="price green">+₹4000</div>
+            <div className="dateTime">2024-05-17 01:00</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">Iphone</div>
+            <div className="description">it was time for a new TV</div>
+          </div>
+          <div className="right">
+            <div className="price red">-₹90000</div>
+            <div className="dateTime">2024-05-17 01:00</div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
